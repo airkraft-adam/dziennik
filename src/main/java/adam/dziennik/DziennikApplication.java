fc19c0d373233e66c003a57dff7ece13a9b12cd2
Adam Przylucki <airkraft@poczta.onet.pl>
2020-05-20 11:16:51 +0200

    add desctop Api

--- src/main/java/adam/dziennik/DziennikApplication.java
+++ src/main/java/adam/dziennik/DziennikApplication.java
@@ -3,11 +3,25 @@ package adam.dziennik;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+import java.awt.*;
+
+
 @SpringBootApplication
 public class DziennikApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DziennikApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DziennikApplication.class, args);
+        System.setProperty("java.awt.headless", "false");
+
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                Okienko okienko = new Okienko();
+                okienko.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                okienko.setVisible(true);
+            }
+            });
+    }
+
 
 }
